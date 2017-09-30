07436ecb0d424672e00cbd6e1b7c69522f84063c
York Tsai <ynntk4815@gmail.com>
2017-09-30 14:19:16 +0800

    add char

--- char.js
+++ char.js
@@ -9,6 +9,7 @@ function getCharSkillDetail(charT) {
     var text = [];
     var skillEffect = getSkillByLevel(charT.skill.effect, charT.c.skillLevel);
     if (charT.id == "183") text.push(getCharSkillDetailId183(skillEffect, detailText));
+    if (charT.id == "188") text.push(getCharSkillDetailId188(skillEffect, detailText));
     if (charT.id == "1001") text.push(getCharSkillDetailId1001(skillEffect, detailText));
     if (charT.id == "1002") text.push(getCharSkillDetailId1002(skillEffect, detailText));
     return text;
@@ -16,6 +17,10 @@ function getCharSkillDetail(charT) {
 
 function getCharSkillDetailId183(skillEffect, detailText) {
     return detailText.format(skillEffect.attack.val, skillEffect.reducedDamage.val, skillEffect.time.val);
+}
+
+function getCharSkillDetailId188(skillEffect, detailText) {
+    return detailText.format(skillEffect.shield.val, skillEffect.time.val);
 }
 
 function getCharSkillDetailId1001(skillEffect, detailText) {
